f07973bbf565c910e0f15a58f8724f1ebfada627
Nat Hamilton <info@nathamilton.com>
2018-12-07 09:20:32 -0600

    chore: clean up dir structor, better build, repo files

--- app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var utils_1 = require("./lib/utils");
-function hey(word) {
-    return "hey " + word;
-}
-console.log(hey("dude"), utils_1.activeDate({ startDate: new Date("2018-12-01"), endDate: new Date("2018-12-10") }));
--- main.ts
+++ main.ts
@@ -4,4 +4,4 @@ function hey(word: string): string {
   return `hey ${word}`;
 }
 
-console.log(hey("dude"), activeDate({ startDate: new Date("2018-12-01"), endDate: new Date("2018-12-10")}));
+console.log(hey("duder"), activeDate({ startDate: new Date("2018-12-01"), endDate: new Date("2018-12-10")}));
